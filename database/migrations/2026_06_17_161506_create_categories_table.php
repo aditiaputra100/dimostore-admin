e7e1380990ee5264fc60dc850726c9b7a2900892
Milda Aditia Putra <aditia.putra100@gmail.com>
2026-06-17 23:25:55 +0700

    add categories migration and model

---
+++ database/migrations/2026_06_17_161506_create_categories_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('parent_id')->nullable();
+            $table->string('name', 100);
+            $table->string('slug', 120);
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->tinyInteger('is_active')->default(1);
+            $table->timestamp('deleted_at');
+            $table->timestamps();
+        });
+
+        Schema::table('categories', function(Blueprint $table) {
+            $table->foreign('parent_id')
+                    ->references('id')
+                    ->on('categories')
+                    ->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categories', function(Blueprint $table) {
+            $table->dropForeign(['parent_id']);
+        });
+
+        Schema::dropIfExists('categories');
+    }
+};
